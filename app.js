3280142b3f52a21e9565f0e25f79f2d3652a7144
Dmytro2708 <130864203+Dmytro2708@users.noreply.github.com>
2024-01-25 21:48:29 +0200

    hw-3

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
 
 const contactsRouter = require("./routes/contactsRouter.js");
 
-const app = express();
+const DB_HOST =
+  "mongodb+srv://Dmytro2708:IRKXrgZC3f9yqE2I@cluster0.vbt9nua.mongodb.net";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 app.use(morgan("tiny"));
 app.use(cors());
@@ -19,8 +34,4 @@ app.use((_, res) => {
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
-});
-
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
 });
